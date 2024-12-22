8108da3ca7af47175d92c0a0b092edeedd6b48d1
kaushik <kyadunath@gmail.com>
2024-12-22 12:11:05 +0800

    Initial Commit

---
+++ HabitHero/HabitHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitHeroApp.swift
+//  HabitHero
+//
+//  Created by kaushik  on 22/12/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
